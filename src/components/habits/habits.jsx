9d1d43a02cd5447edef8cd17fb616e5900e9f031
sunny-yo <dmsfh3@gmail.com>
2021-11-17 20:51:00 +0900

    change to function Component: habits

--- src/components/habits/habits.jsx
+++ src/components/habits/habits.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./habits.module.css";
 import Habit from "../habit/habit";
 import AddForm from "../add-form/add-form";
 
-class Habits extends Component {
-  render() {
-    return (
-      <div className={styles.habits}>
-        <AddForm onAdd={this.props.onAdd} />
-        <ul>
-          {this.props.habits.map((habit) => (
-            <Habit
-              key={habit.id}
-              habit={habit}
-              onIncrement={this.props.onIncrement}
-              onDecrement={this.props.onDecrement}
-              onDelete={this.props.onDelete}
-            />
-          ))}
-        </ul>
-        <button className={styles.resetBtn} onClick={this.props.onReset}>
-          Reset All
-        </button>
-      </div>
-    );
-  }
-}
+const Habits = (props) => {
+  return (
+    <div className={styles.habits}>
+      <AddForm onAdd={props.onAdd} />
+      <ul>
+        {props.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={props.onIncrement}
+            onDecrement={props.onDecrement}
+            onDelete={props.onDelete}
+          />
+        ))}
+      </ul>
+      <button className={styles.resetBtn} onClick={props.onReset}>
+        Reset All
+      </button>
+    </div>
+  );
+};
 
 export default Habits;
