068525d87a43cbd38a23f701d57fcf16675e4cb3
sunny-yo <dmsfh3@gmail.com>
2021-11-17 14:45:15 +0900

    change to function Component: navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 import styles from "./navbar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-class Navbar extends PureComponent {
-  render() {
-    return (
-      <nav className={styles.container}>
-        <FontAwesomeIcon icon={faCheck} className={styles.icon} />
-        <span className={styles.title}>Habit Tracker</span>
-        <span className={styles.count}>{this.props.totalCount}</span>
-      </nav>
-    );
-  }
-}
+const Navbar = memo((props) => {
+  return (
+    <nav className={styles.container}>
+      <FontAwesomeIcon icon={faCheck} className={styles.icon} />
+      <span className={styles.title}>Habit Tracker</span>
+      <span className={styles.count}>{props.totalCount}</span>
+    </nav>
+  );
+});
 
 export default Navbar;
