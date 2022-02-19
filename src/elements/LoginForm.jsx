9b52f0b2c732b2f115338326e9923b7063986a2e
sunny-yo <dmsfh3@gmail.com>
2022-02-19 11:42:28 +0900

    Feat check validation of register&login

--- src/elements/LoginForm.jsx
+++ src/elements/LoginForm.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../components/Button';
-import LabelInput from '../components/LabelInput';
+import { checkEmail } from '../shared/functions';
 
 const LoginForm = (props) => {
+  const navigate = useNavigate();
+  const emailRef = useRef();
+  const pwRef = useRef();
+
+  const onLogin = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const pw = pwRef.current.value;
+
+    if (!checkEmail(email).res) {
+      emailRef.current.focus();
+      alert(checkEmail(email).msg);
+      return;
+    }
+
+    if (pw === '') {
+      pwRef.current.focus();
+      alert('비밀번호를 입력해주세요');
+    }
+
+    const loginData = {
+      email: email,
+      password: pw,
+    };
+    console.log(loginData);
+    // GET /api/login
+    // response => msg 띄워주기
+    navigate('/'); // 성공일 때
+  };
+
   return (
-    <Form>
-      <LabelInput name={'아이디'} placeholder={'아이디를 입력하세요'} />
-      <LabelInput name={'비밀번호'} placeholder={'비밀번호를 입력하세요'} />
+    <Form onSubmit={onLogin}>
+      <Box>
+        <Label htmlFor="이메일">이메일</Label>
+        <Input ref={emailRef} type="text" placeholder="이메일을 입력하세요" />
+      </Box>
+      <Box>
+        <Label htmlFor="비밀번호">비밀번호</Label>
+        <Input ref={pwRef} type="text" placeholder="비밀번호를 입력하세요" />
+      </Box>
       <Button name={'로그인하기'} />
     </Form>
   );
@@ -16,6 +54,23 @@ const LoginForm = (props) => {
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5em 0;
+`;
+
+const Label = styled.label`
+  font-size: 0.7rem;
+  font-weight: bold;
+  margin-bottom: 0.2em;
+`;
+
+const Input = styled.input`
+  font-size: 1rem;
+  padding: 0.3em 0.1em;
 `;
 
 export default LoginForm;
