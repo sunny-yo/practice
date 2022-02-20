dbe15a02fe619e791346babf8bd9d26494613ff8
sunny-yo <dmsfh3@gmail.com>
2022-02-20 17:37:51 +0900

    Feat: add 'like post' functionality (add redux post module)

--- src/components/PostCardFooter.jsx
+++ src/components/PostCardFooter.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { FaHeart } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { postLike, postLikeCancel } from '../redux/modules/post';
 
-const PostCardFooter = (props) => {
+const PostCardFooter = ({ like, likes, boardId }) => {
+  const isLogin = useSelector((state) => state.user.is_login);
+  const userId = useSelector((state) => state.user.userId);
+  const dispatch = useDispatch();
+  const [onLike, setOnLike] = useState(false);
+
+  // 로그인한 유저가 게시글을 좋아요한 리스트에 있으면 onLike=true
+  useEffect(() => {
+    if (likes.filter((user) => user.userId === userId).length > 0) {
+      setOnLike(true);
+    }
+  }, []);
+
+  const handleLike = (e) => {
+    if (!isLogin) {
+      alert('로그인이 필요합니다');
+      e.stopPropagation();
+      return;
+    }
+    if (e.currentTarget.id === 'like-button' && !onLike) {
+      e.currentTarget.classList.toggle('like');
+      e.stopPropagation();
+      setOnLike(true);
+      dispatch(postLike({ boardId, userId }));
+    } else if (e.currentTarget.id === 'like-button' && onLike) {
+      e.currentTarget.classList.toggle('like');
+      e.stopPropagation();
+      setOnLike(false);
+      dispatch(postLikeCancel({ boardId, userId }));
+    }
+  };
+
   return (
     <FooterBox>
-      <Like>
       <span>좋아요 {like}개</span>
+      <Like id="like-button" onClick={handleLike} className={onLike && 'like'}>
         <FaHeart />
       </Like>
     </FooterBox>
@@ -26,6 +59,9 @@ const Like = styled.span`
   cursor: pointer;
   transition: all 120ms ease-in;
   &:hover {
+    color: pink;
+  }
+  &.like {
     color: pink;
   }
 `;
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './modules/user';
+import postReducer from './modules/post';
 import { createBrowserHistory } from 'history';
 import { connectRouter } from 'connected-react-router';
 
 export const history = createBrowserHistory();
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: { user: userReducer, post: postReducer },
   router: connectRouter(history),
 });
